1e9071174ccd59bcfa3f2ca3247275739418d21c
robfanio <fanio1981@gmail.com>
2021-09-26 11:30:35 -0400

    first commit

---
+++ Landing_Page/src/main/java/Finpay_V1/Landing_Page/LandingPage.java
@@ -0,0 +1,30 @@
+package Finpay_V1.Landing_Page;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * Hello world!
+ *
+ */
+public class LandingPage 
+{
+    public static void main( String[] args )
+    {
+      WebDriverManager.chromedriver().setup();
+      WebDriver driver = new ChromeDriver();
+      
+      driver.get("https://www.finpay.com");
+      driver.manage().window().maximize();
+      driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+      WebElement abtNav= driver.findElement(By.xpath("//ul[@class='submenu level-1']/li[1]"));
+      abtNav.click();
+      
+    }
+}
---
+++ Landing_Page/src/main/java/Finpay_V1/Landing_Page/Practice1.java
@@ -0,0 +1,10 @@
+package Finpay_V1.Landing_Page;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
